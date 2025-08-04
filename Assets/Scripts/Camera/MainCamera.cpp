3c81c7109fecc05e4007664c7f9accd28ec68a49
tochouseito <146930194+tochouseito@users.noreply.github.com>
2025-08-04 10:26:10 +0900

    インクルード追加

--- Assets/Scripts/Camera/MainCamera.cpp
+++ Assets/Scripts/Camera/MainCamera.cpp
@@ -1,6 +1,11 @@
 #include "MainCamera.h"
 using namespace ChoSystem;
-#include "../Player.h"
+#include "../Player/PlayerBullet.h"
+#include "../Player/PlayerMisslile.h"
+#include "../Enemy/EnemyBullet.h"
+#include "../Enemy/EnemyMissile.h"
+#include "../FieldObject/Wall.h"
+#include "../FieldObject/Ground.h"
 
 void MainCamera::Start()
 {
