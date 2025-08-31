c85857e22fd211856ebba9d0e06ca618c3cabdf6
toto <k023g0060@m.neec.ac.jp>
2025-08-31 22:09:23 +0900

    シーン切り替え実装

---
+++ Assets/Scripts/Player/TitlePlayerAnimation.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Marionnette.h"
+
+class TitlePlayerAnimation : public Marionnette
+{
+public:
+	// コンストラクタ
+    TitlePlayerAnimation(GameObject& object) : Marionnette(object) {}
+    // 初期化処理
+    void Start() override;
+	// 毎フレーム処理
+    void Update() override;
+private:
+    float m_Amplitude = 6.0f;   // 揺れ幅（上下合計で 24）
+    float m_FrequencyHz = 1.0f;    // 1秒に1往復
+    float m_Phase = 0.0f;    // 現在の位相[rad]
+    Vector3 m_BasePos{};        // 揺れの基準位置
+};
